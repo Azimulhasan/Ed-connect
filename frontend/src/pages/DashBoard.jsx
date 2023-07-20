180a42b93bfb31673f714d932fe75d2c37141d78
Zarn D. Arc <azimulhasan.rocks>
2023-07-20 19:06:32 +0530

    did some work on dashboard with darkmode

--- frontend/src/pages/DashBoard.jsx
+++ frontend/src/pages/DashBoard.jsx
@@ -1,15 +1,31 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { PageContext } from '../context'
 import { useNavigate } from 'react-router-dom'
+import { Sidebar , Navbar } from '../components'
 
 const Dashboard = () => {
   const navigate = useNavigate()
   const { isAuthenticated } = useContext(PageContext)
-  if(!isAuthenticated){
-    useNavigate("/")
-  }
+  const [darkMode, setDarkMode] = useState(true)
+  
+  useEffect(()=>{
+    if(!isAuthenticated){
+      console.log("not auth")
+  
+    }
+  },[])
   return (
-   <div>DashBoard</div>
+    <div className={`relative sm:p-8 p-4 bg-[color:var(${darkMode?'--darkmode-bg-color':'--lightmode-bg-color'})] min-h-screen flex flex-row`}>
+    <div className='sm:flex hidden mr-10 relative'>
+      <Sidebar darkMode={darkMode} setDarkMode={setDarkMode} />
+    </div>
+
+    <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
+      <Navbar />
+
+
+    </div>
+  </div>
   )
 }
 
